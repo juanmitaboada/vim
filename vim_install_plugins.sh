9adee029c1a3b8f8e7a47c8db9b12cf0c74d6657
Juanmi Taboada <juanmi@juanmitaboada.com>
2023-03-04 08:03:11 +0100

    Separate packages

--- vim_install_plugins.sh
+++ vim_install_plugins.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # flake8 vim-khuno
-# python3-autopep8 python3-pep8 python3-pytest-pep8 vim-autopep8
+# python-autopep8 python3-autopep8 python3-pep8 python3-pytest-pep8 vim-autopep8
 # pylama
 # python3-pylama
 # python3-flake8-polyfill python3-pep8-naming
 # pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes
 # python3-pylint-celery python3-pylint-common python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils
 
-sudo apt-get $1 $2 $3 $4 install flake8 vim-khuno python3-autopep8 python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-celery python3-pylint-common python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils
+packages="flake8 vim-khuno python-autopep8 python3-autopep8 python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-celery python3-pylint-common python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils"
+
+for package in $packages ; do
+    sudo apt-get $1 $2 $3 $4 install $package
+done
