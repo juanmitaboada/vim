02c578d4911c4cdfcbb9a644cd70b2acde24ee13
Juanmi Taboada <juanmi@juanmitaboada.com>
2023-03-04 08:08:44 +0100

    Separte install

--- vim_install_plugins.sh
+++ vim_install_plugins.sh
@@ -8,8 +8,9 @@
 # pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes
 # python3-pylint-celery python3-pylint-common python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils
 
-packages="flake8 vim-khuno python-autopep8 python3-autopep8 python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-celery python3-pylint-common python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils"
+packages="vim-nox flake8 vim-khuno python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils"
 
-for package in $packages ; do
-    sudo apt-get $1 $2 $3 $4 install $package
-done
+sudo apt-get $1 $2 $3 $4 install $packages
+sudo apt-get $1 $2 $3 $4 install python3-autopep8 || sudo apt-get $1 $2 $3 $4 install python-autopep8
+sudo apt-get $1 $2 $3 $4 install python3-pylint-celery
+sudo apt-get $1 $2 $3 $4 install python3-pylint-common
