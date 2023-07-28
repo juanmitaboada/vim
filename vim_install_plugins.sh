01d201ea6256e2bbb417b5693728b100de075442
Juanmi Taboada <juanmi@juanmitaboada.com>
2023-07-28 09:22:23 +0200

    Auto install

--- vim_install_plugins.sh
+++ vim_install_plugins.sh
@@ -11,12 +11,12 @@
 packages="vim-nox flake8 vim-khuno python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-django python3-pylint-plugin-utils make exuberant-ctags jq ack shellcheck"
 
 # shellcheck disable=SC2086
-sudo apt-get $1 $2 $3 $4 install "$packages"
+sudo apt-get $1 $2 $3 $4 install -y "$packages"
 # shellcheck disable=SC2086
-sudo apt-get $1 $2 $3 $4 install python3-pylint-flask
+sudo apt-get $1 $2 $3 $4 install -y python3-pylint-flask
 # shellcheck disable=SC2086
-sudo apt-get $1 $2 $3 $4 install python3-autopep8 || sudo apt-get $1 $2 $3 $4 install python-autopep8
+sudo apt-get $1 $2 $3 $4 install -y python3-autopep8 || sudo apt-get $1 $2 $3 $4 install python-autopep8
 # shellcheck disable=SC2086
-sudo apt-get $1 $2 $3 $4 install python3-pylint-celery
+sudo apt-get $1 $2 $3 $4 install -y python3-pylint-celery
 # shellcheck disable=SC2086
-sudo apt-get $1 $2 $3 $4 install python3-pylint-common
+sudo apt-get $1 $2 $3 $4 install -y python3-pylint-common
