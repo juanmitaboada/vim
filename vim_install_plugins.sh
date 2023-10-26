29bdb1dc13a2f944cf24fdeeb14e3a9fd937d6ed
Juanmi Taboada <juanmi@juanmitaboada.com>
2023-10-26 20:21:12 +0200

    Bug corrected on installer

--- vim_install_plugins.sh
+++ vim_install_plugins.sh
@@ -11,7 +11,7 @@
 packages="vim-nox flake8 vim-khuno python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-django python3-pylint-plugin-utils make exuberant-ctags jq ack shellcheck cmake black"
 
 # shellcheck disable=SC2086
-sudo apt-get $1 $2 $3 $4 install -y "$packages"
+sudo apt-get $1 $2 $3 $4 install -y $packages
 # shellcheck disable=SC2086
 sudo apt-get $1 $2 $3 $4 install -y python3-pylint-flask
 # shellcheck disable=SC2086
