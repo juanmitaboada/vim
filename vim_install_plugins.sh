9d1e61b3d797e64f3178af4c6fbfc3a08edb2491
Juanmi Taboada <juanmi@juanmitaboada.com>
2025-05-30 19:03:02 +0200

    Python3 headers for YouCompleteMe

--- vim_install_plugins.sh
+++ vim_install_plugins.sh
@@ -8,7 +8,7 @@
 # pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes
 # python3-pylint-celery python3-pylint-common python3-pylint-django python3-pylint-flask  python3-pylint-plugin-utils
 
-packages="vim-nox flake8 vim-khuno python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-plugin-utils make exuberant-ctags jq ack shellcheck cmake black"
+packages="vim-nox flake8 vim-khuno python3-pep8 python3-pytest-pep8 vim-autopep8 pylama python3-pylama python3-flake8-polyfill python3-pep8-naming pyflakes3 python3-flake8 python3-flake8-polyfill python3-pyflakes python3-pylint-plugin-utils make exuberant-ctags jq ack shellcheck cmake black python3-dev"
 
 # shellcheck disable=SC2086
 sudo apt-get $1 $2 $3 $4 install -y $packages
